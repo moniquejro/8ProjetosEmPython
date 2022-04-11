c0a93fe978472f16a6b3592ffabdb9ff01b8ca8a
moniquejro <moniquejroliveira@gmail.com>
2022-04-11 15:38:53 -0300

    First project of eight, with window and button

--- SimuladorDado.py
+++ SimuladorDado.py
@@ -1,20 +1,29 @@
 # Simulador de dado
-# Simular o uso de um dado, gerando um valor aleatório entre os numeros 1 e 6.
-
+# Simular o uso de um dado, gerando um valor aleatório entre os numeros 1 até 6
 import random
+import PySimpleGUI as sg
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Gostaria de gerar um novo valor?'
+        # Layout
+        self.layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('sim'),sg.Button('nao')]
+        ]
 
     def Iniciar(self):
-        resposta = input(self.mensagem)
+        # Criar uma janela
+        self.janela = sg.Window('Simulador de Dado',layout=self.layout)
+        # Ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        # Fazer alguma coisa com esses valores
         try:
-            if resposta == 'sim' or resposta == 's':
+            self.eventos == 'sim'
+            if self.eventos == 'sim' or self.eventos == 's':
                 self.GerarValorDoDado()
-            elif resposta == 'não' or resposta == 'n' or resposta == 'nao':
+            elif self.eventos == 'não' or self.eventos == 'n' or self.eventos == 'nao':
                 print('Agradecemos a sua participacao!')
             else:
                 print('Favor digitar sim ou não')
